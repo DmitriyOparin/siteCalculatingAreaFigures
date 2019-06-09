cef14a187cb81ce42b20ded4cc932a18da0d7cb8
DmitriyOparin <my@mail.ru>
2019-06-09 13:03:14 +0300

    fix unit tests

--- src/test/java/ru/ngtu/ci/tests/UnitTests.java
+++ src/test/java/ru/ngtu/ci/tests/UnitTests.java
@@ -8,38 +8,50 @@ public class UnitTests {
     @Test
     public void squareRectangleTest() {
         CalculationSquare calculationSquare = new CalculationSquare();
-        String result = calculationSquare.calculationSquareRectangle("2", "2");
-        System.out.println(result);
-        Assert.assertEquals("4,0", result);
+        String expected_result = "4,0";
+        String actual_result = calculationSquare.calculationSquareRectangle("2", "2");
+        System.out.println(actual_result);
+//        Assert.assertEquals("4,0", result);
+        Assert.assertEquals(expected_result, actual_result);
     }
 
     @Test
     public void squareZeroRectangleTest() {
         CalculationSquare calculationSquare = new CalculationSquare();
-        String result = calculationSquare.calculationSquareRectangle("0", "0");
-        System.out.println(result);
-        Assert.assertEquals("0,0", result);
+        String expected_result = "0,0";
+        String actual_result = calculationSquare.calculationSquareRectangle("0", "0");
+        System.out.println(actual_result);
+//        Assert.assertEquals("0,0", result);
+        Assert.assertEquals(expected_result, actual_result);
     }
 
     @Test
     public void squareCircleTest() {
         CalculationSquare calculationSquare = new CalculationSquare();
-        String result = calculationSquare.calculationSquareCircle("2");
-        System.out.println(result);
-        Assert.assertEquals("12,6", result);
+        String expected_result = "12,6";
+        String actual_result = calculationSquare.calculationSquareCircle("2");
+        System.out.println(actual_result);
+//        Assert.assertEquals("12,6", result);
+        Assert.assertEquals(expected_result, actual_result);
     }
 
     @Test
     public void squareZeroCircleTest() {
         CalculationSquare calculationSquare = new CalculationSquare();
-        String result = calculationSquare.calculationSquareCircle("0");
-        System.out.println(result);
-        Assert.assertEquals("0,0", result);
+        String expected_result = "0,0";
+        String actual_result = calculationSquare.calculationSquareCircle("0");
+        System.out.println(actual_result);
+        Assert.assertEquals(expected_result, actual_result);
     }
 
     @Test
     public void testTest() {
         System.out.println("result");
         Assert.assertEquals("result", "result");
+
+
+
+
+
     }
 }
